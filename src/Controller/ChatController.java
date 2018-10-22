3ea86f8e12fa42ecb2f8455edf0a46b10d9f841c
Emilysavannahjames <43076722+Emilysavannahjames@users.noreply.github.com>
2018-10-22 12:12:00 -0600

    many errors- added two test JUNIT

---
+++ src/Controller/ChatController.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import Model.Chatbot;
+
+import javax.swing.JOptionPane;
+
+public class Chatbot
+{
+
+	private Chatbot simpleBot;
+	
+	public ChatController()
+	{
+		simpleBot = new Chatbot();
+	}
+	
+	public void start()
+	{
+		String userInput = "";
+		
+		while (!userInput.equalsIgnoreCase("quit"))
+		{
+			userInput = interactWithChatbot(userInput);
+		}
+	}
+	
+	public String interactWithChatbot(String text)
+	{
+		String output = "";
+		String userResponse = JOptionPane.showInputDialog(null, "Hi, what's up?");
+		output = simpleBot.processText(userResponse);
+		return output;
+	}
+	
+	public String processText(String userText)
+	{
+		String answer = "";
+		answer += "You said: " + userText;
+		return answer;
+	}
+	public Chatbot getChatebot()
+	{
+		return simpleBot;
+	}
+	
+	
+}
+	
+	
+	
+	
+}
--- src/Model/Chatbot.java
+++ src/Model/Chatbot.java
@@ -1,8 +1,11 @@
 package Model;
+import java.util.ArrayList;
 
 public class Chatbot
 {
 	//Declaration Section
+	private ArrayList<String> spookyList;
+	private ArrayList<String> responseList;
 	private String content;
 	private String currentUser;
 	private String joke;	
@@ -16,55 +19,38 @@ public class Chatbot
 	
 	public Chatbot()
 	{
-		//constructor default
+		this.joke = "Why did the chicken cross the road?";
+		this.content = new String("Boring content drab empty blah");
+		this.currentUser = new String("This is default user");
+		
+		this.responseList = new ArrayList<String>();
+		this.spookyList = new ArrayList<String>();
+		
+		buildTheLists();
 	}
 	
-	/**
-	 * Initializes a name 
-	 * @param name or the run
-	 */
-	
-	public Chatbot(String content)
+	private void buildTheLists()
 	{
-		this.content = content;
+		responseList.add("Hello! How are you?");
+		responseList.add("Goodbye - no more talking!");
+		responseList.add("What is your name?");
+		responseList.add("What is your favorite color?");
+		responseList.add("Dumbo");
+		
+		spookyList.add("Happy Halloween!");
+		spookyList.add("BOO!!!");
+		spookyList.add("What are you dressing up as?");
 	}
 	
-	public Chatbot(String content, String currentUser, String joke)
+	public String processText(String userText)
 	{
-		this.content = content;
-		this.currentUser = currentUser;
-		this.joke = joke;
+		String answer = "";
+		answer += "You said: " + userText;
+		return answer;
 	}
 	
-	public String getContent()
-	{
-		return content;
-	}
 	
-	public String getCurrentUser()
-	{
-		return currentUser;
-	}
 	
-	public String getJoke()
-	{
-		return joke;
-	}
-	
-	public void setContent(String content)
-	{
-		this.content = content;
-	}
-	
-	public void setCurrentUser(String currentUser)
-	{
-		this.currentUser = currentUser;
-	}
-	
-	public void setJoke(String joke)
-	{
-		this.joke = joke;
-	}
 	
 }
 
